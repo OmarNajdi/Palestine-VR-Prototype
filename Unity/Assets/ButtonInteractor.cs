e82f12779b9fb62344884e91f1c6ec83dda2683f
Omar Najdi <omarnajdi@gmail.com>
2018-08-28 09:32:35 +0200

    Unity First Commit
    *Unity*
    VR Video
    Button
    Gaze

---
+++ Unity/Assets/ButtonInteractor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonInteractor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Update");
+    }
+
+    public void PointerExit()
+    {
+        Debug.Log("Pointer Exit");
+    }
+
+    public void PointerEnter()
+    {
+        Debug.Log("Pointer Enter");
+        SceneManager.LoadScene("scene-menu");
+   }
+
+    public void PointerDown()
+    {
+        Debug.Log("Pointer Down");
+    }
+}
---
+++ Unity/Assets/InteractiveItem.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+[RequireComponent(typeof(Button))]
+public class InteractiveItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Image progressImage; // add an image as child to your button object and set its image type to Filled. Assign it to this field in inspector.
+    public bool isEntered = false;
+    RectTransform rt;
+    Button _button;
+    float timeElapsed;
+    Image cursor;
+    // Use this for initialization
+    void Awake()
+    {
+        _button = GetComponent<Button>();
+        rt = GetComponent<RectTransform>();
+
+    }
+
+    float GazeActivationTime = 3;
+
+    void Update()
+    {
+        if (isEntered)
+        {
+            timeElapsed += Time.deltaTime;
+            progressImage.fillAmount = Mathf.Clamp(timeElapsed / GazeActivationTime, 0, 1);
+            if (timeElapsed >= GazeActivationTime)
+            {
+                timeElapsed = 0;
+                _button.onClick.Invoke();
+                progressImage.fillAmount = 0;
+                isEntered = false;
+            }
+        }
+        else
+        {
+            timeElapsed = 0;
+        }
+    }
+
+    #region IPointerEnterHandler implementation
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isEntered = true;
+    }
+
+    #endregion
+
+    #region IPointerExitHandler implementation
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isEntered = false;
+        progressImage.fillAmount = 0;
+    }
+
+}
+#endregion
